74d4bf9f714f32e3cd6cb0f6222393a44734d21d
davendra <davendra@intellectstorm.com>
2017-05-20 15:39:30 -0400

    Edit Book function

---
+++ booksCtrl.js
@@ -0,0 +1,39 @@
+app.controller('booksController', function($scope, $http) {
+		console.log("Books here");
+	
+		$scope.addBook = function(book) {
+			$scope.books.push(book);
+		}
+		
+		$scope.getBooks = function() {
+			$http({
+				"url": "/librarymanagementsystem/index.php/v1/books",
+				"method": "GET",
+			}).then(function(success_response) {
+				$scope.books = success_response.data
+			}, function(error_response) {
+				console.error(error_response);
+			}).finally(function() {
+				console.log("Done");
+			})
+		}
+		
+		$scope.getBooks();
+	
+		$scope.edit_book = function(book) {
+			$http({
+				url: "/librarymanagementsystem/index.php/v1/books/" + book.book_id,
+				method: "PATCH",
+				data: {
+						ug_id: book.ug_id,
+						condition_id: book.condition_id
+				}
+			}).then(function(success) {
+					console.log("Good");
+			}, function(error) {
+					console.error("Bad");
+			}).finally(function() {
+					$scope.getBooks();
+			})
+		}
+  });
