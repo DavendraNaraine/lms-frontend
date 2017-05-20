e5f2e7af53c003640cf4b3041d522250fab20327
davendra <davendra@intellectstorm.com>
2017-05-20 15:01:59 -0400

    angular get book

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-angular.module('lmsApp', [])
-  .controller('booksController', function($scope, $http) {
-		$scope.addBook = function(book) {
-			$scope.books.push(book);
-		}
-		
-		$scope.getBooks = function() {
-			$http({
-				"url": "/librarymanagementsystem/index.php/v1/books",
-				"method": "GET",
-			}).then(function(success_response) {
-				$scope.books = success_response.data
-			}, function(error_response) {
-				console.error(error_response);
-			}).finally(function() {
-				console.log("Done");
-			})
-		}
-		
-		$scope.getBooks();
-  });
+var app = angular.module('lmsApp', ['ui.router'])
+.config(function($stateProvider) {
+  var bookState = {
+    name: 'books',
+    url: '/books',
+    templateUrl: 'books.html',
+		controller: "booksController"
+  }
+
+  $stateProvider.state(bookState);
+});
